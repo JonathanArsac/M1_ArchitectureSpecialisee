eceb75451029da1d74d0b42d45679967b1c2a772
Jaune <jonathanarsac@orange.fr>
2020-01-25 11:51:48 +0100

    Fin projet

--- TP_exemple.cc
+++ TP_exemple.cc
@@ -25,16 +25,16 @@ void affiche_xmm_i ( __m128i x )
 {
   uint32_t a[4];
   _mm_store_si128((__m128i*)a,x);
-  cout<<"Xmm = "<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" \n";
+  cout<<"Xmm 32 = "<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" \n";
 
   uint16_t a2[8];
   _mm_store_si128((__m128i*)a2,x);
-  cout<<"Xmm = "<<a2[0]<<" "<<a2[1]<<" "<<a2[2]<<" "<<a2[3]<<" "
+  cout<<"Xmm 16 = "<<a2[0]<<" "<<a2[1]<<" "<<a2[2]<<" "<<a2[3]<<" "
   <<a2[4]<<" "<<a2[5]<<" "<<a2[6]<<" "<<a2[7]<<" \n";
 
   uint8_t a3[16];
   _mm_store_si128((__m128i*)a3,x);
-  cout<<"Xmm = "<<(int)a3[0]<<" "<<(int)a3[1]<<" "<<(int)a3[2]<<" "<<(int)a3[3]<<" "
+  cout<<"Xmm 8 = "<<(int)a3[0]<<" "<<(int)a3[1]<<" "<<(int)a3[2]<<" "<<(int)a3[3]<<" "
   <<(int)a3[4]<<" "<<(int)a3[5]<<" "<<(int)a3[6]<<" "<<(int)a3[7]<<" "
   <<(int)a3[8]<<" "<<(int)a3[9]<<" "<<(int)a3[10]<<" "<<(int)a3[11]<<" "
   <<(int)a3[12]<<" "<<(int)a3[13]<<" "<<(int)a3[14]<<" "<<(int)a3[15]<<" \n";
@@ -64,6 +64,7 @@ void Inverse_sse(unsigned char *X, long h, long l, unsigned char *Y)
   b=_mm_set1_epi8(255);
   for(int i=0;i<nbPixels;i+=16){
     a=_mm_load_si128((__m128i*)&X[i]);
+    // on soustrait à 255 la valeur donnée
     c=_mm_sub_epi8(b,a);
     _mm_store_si128((__m128i*)&Y[i],c);
   }
@@ -82,10 +83,11 @@ void Luminance_sse(int d_lum, unsigned char *X, long h, long l, unsigned char *Y
   __m128i a,b,c;
   if(d_lum<0){
     b=_mm_set1_epi8(abs(d_lum));
-    affiche_xmm_i(b);
+    //affiche_xmm_i(b);
 
     for(int i=0;i<nbPixels;i+=16){
        a=_mm_load_si128((__m128i*)&X[i]);
+       // on soustrait la luminance en faisant attention à la saturation
        c=_mm_subs_epu8(a,b);
        _mm_store_si128((__m128i*)&Y[i],c);
      }
@@ -94,6 +96,7 @@ void Luminance_sse(int d_lum, unsigned char *X, long h, long l, unsigned char *Y
     b=_mm_set1_epi8(d_lum);
     for(int i=0;i<nbPixels;i+=16){
        a=_mm_load_si128((__m128i*)&X[i]);
+       // on ajoute la luminance si elle est positive
        c=_mm_adds_epu8(a,b);
        _mm_store_si128((__m128i*)&Y[i],c);
      }
@@ -128,6 +131,7 @@ void binarise_sse (unsigned char seuil, unsigned char *X, long h, long l, unsign
   __m128i v_donnees,v_resultat;
   for(int i=0;i<nbPixels;i+=16){
     v_donnees=_mm_load_si128((__m128i*)&X[i]);
+    // trick pour faire un cmpgt en int donc on va utiliser un ou exclusief sur un masque résultant de la comparaison entre le seuil et la valeur max entre le seuil et la donnée
     v_resultat=_mm_xor_si128(_mm_cmpeq_epi8(_mm_max_epu8(v_seuil,v_donnees),v_seuil),v_moins1);
     _mm_store_si128((__m128i*)&Y[i],v_resultat);
   }
@@ -159,17 +163,25 @@ void min_et_max_sse (unsigned char *X, long h, long l, unsigned char & min, unsi
 
   int nbPixels = h*l;
   __m128i v_min = _mm_set1_epi8(255);
-    __m128i v_max = _mm_set1_epi8(0);
-    __m128i v_donnees;
+  __m128i v_max = _mm_set1_epi8(0);
+  __m128i v_donnees;
+  min = 255;
+  max = 0;
 
   for(int i=0;i<nbPixels;i+=16){
     v_donnees=_mm_load_si128((__m128i*)&X[i]);
-    v_min = ;
-    v_max= ;
+    v_min = _mm_min_epu8(v_min,v_donnees) ;
+    v_max= _mm_max_epu8(v_max,v_donnees) ;
   }
+  unsigned char tmpMin[16];
+  unsigned char tmpMax[16];
+  _mm_store_si128((__m128i*)&tmpMin[0],v_min);
+  _mm_store_si128((__m128i*)&tmpMax[0],v_max);
 
+// on cherche le min entre tout le vecteur de 16 entiers
   for(int i=0;i<16;i++){
-    min = min(tmpMin[i],min);
+    min = std::min(tmpMin[i],min);
+    max = std::max(tmpMax[i],max);
   }
 
 
@@ -195,8 +207,77 @@ void recadrageDynamique (unsigned char *X, long h, long l, unsigned char min, un
     }
 }
 
+
+
 void recadrageDynamique_sse(unsigned char *X, long h, long l, unsigned char min, unsigned char max, unsigned char *Y)
 {
+
+  long nbPixels=h*l;
+
+__m128i v_donnees;
+__m128i v_min = _mm_set1_epi8(min);
+__m128i v_255 = _mm_set1_epi16(255);
+__m128 v_div = _mm_set1_ps(max-min);
+//__m128i v_255 = _mm_set1_epi16(255);
+__m128i v_donneeslo;
+__m128i v_donneeshi;
+__m128i v_donneeslolo;
+__m128i v_donneeshihi;
+__m128i v_donneeshilo;
+__m128i v_donneeslohi;
+
+__m128 v_donneesloloF;
+__m128 v_donneeshihiF;
+__m128 v_donneeshiloF;
+__m128 v_donneeslohiF;
+
+
+__m128i v_masque = _mm_set_epi8(0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1);
+__m128i v_masque2 = _mm_set_epi16(0,-1,0,-1,0,-1,0,-1);
+  for(long i=0;i<nbPixels;i+=16){
+    v_donnees=_mm_load_si128((__m128i*)&X[i]);
+    v_donnees = _mm_subs_epu8(v_donnees,v_min);
+
+    // séparer en deux vecteurs de forme 0 X 0 X 0 X .... pour pouvoir multiplier sans overflow
+    v_donneeslo = _mm_and_si128(v_donnees,v_masque);
+    v_donneeshi = _mm_and_si128(_mm_srli_si128(v_donnees,1),v_masque);
+    // multiplication  sur 16 bits
+    v_donneeslo = _mm_mullo_epi16(v_donneeslo,v_255);
+    v_donneeshi = _mm_mullo_epi16(v_donneeshi,v_255);
+
+    // transformer le vecteur d'entiers de 16  bits en 32
+    v_donneeshilo = _mm_and_si128(v_donneeshi,v_masque2);
+    v_donneeshihi = _mm_and_si128(_mm_srli_si128(v_donneeshi,2),v_masque2);
+
+    v_donneeslolo = _mm_and_si128(v_donneeslo,v_masque2);
+    v_donneeslohi = _mm_and_si128(_mm_srli_si128(v_donneeslo,2),v_masque2);
+
+// transformation en float pour pouvoir diviser
+    v_donneeshiloF = _mm_cvtepi32_ps(v_donneeshilo);
+    v_donneeshihiF = _mm_cvtepi32_ps(v_donneeshihi);
+    v_donneeslohiF = _mm_cvtepi32_ps(v_donneeslohi);
+    v_donneesloloF = _mm_cvtepi32_ps(v_donneeslolo);
+
+// division sur 32 bits
+    v_donneeshiloF = _mm_div_ps(v_donneeshiloF,v_div);
+    v_donneeshihiF = _mm_div_ps(v_donneeshihiF,v_div);
+    v_donneeslohiF = _mm_div_ps(v_donneeslohiF,v_div);
+    v_donneesloloF = _mm_div_ps(v_donneesloloF,v_div);
+
+// reconversion en entier 32 bits pour pouvoir le store
+    v_donneeshilo = _mm_cvtps_epi32(v_donneeshiloF);
+    v_donneeshihi = _mm_cvtps_epi32(v_donneeshihiF);
+    v_donneeslohi = _mm_cvtps_epi32(v_donneeslohiF);
+    v_donneeslolo = _mm_cvtps_epi32(v_donneesloloF);
+
+// on récupère les données qui sont sous forme 0 0 0 X 0 0 0 Y .... donc on décale un par un d'un octet supplémentaire afin de se retrouver avec X Y Z W
+    v_donnees = _mm_or_si128(_mm_or_si128(_mm_slli_si128(v_donneeshihi,3),_mm_slli_si128(v_donneeslohi,2)),_mm_or_si128(_mm_slli_si128(v_donneeshilo,1),v_donneeslolo));
+
+
+    _mm_store_si128((__m128i*)&Y[i],v_donnees);
+
+
+  }
 }
 
 
@@ -256,10 +337,21 @@ int main (int argc, char** argv)
   min_et_max( (unsigned char*) img->imageData, img->height, img->width, min, max);
   cout << "min : " << (int)min << "   max : " << (int)max << endl;
 
+  unsigned char min2, max2;
+  min_et_max_sse( (unsigned char*) img->imageData, img->height, img->width, min2, max2);
+  cout << "min sse : " << (int)min2 << "   max sse : " << (int)max2 << endl;
+
 	// cr�ation d'une image pour stocker l'image de recadrage dynamique
   IplImage* image_ = cvCreateImage (cvGetSize (img), IPL_DEPTH_8U, 1);
   recadrageDynamique( (unsigned char*) img->imageData, img->height, img->width, min, max, (unsigned char*) image_->imageData);
   cvShowImage( "Affiche_", image_);
+
+
+  	// cr�ation d'une image pour stocker l'image de recadrage dynamique
+    IplImage* image_sse = cvCreateImage (cvGetSize (img), IPL_DEPTH_8U, 1);
+    recadrageDynamique_sse( (unsigned char*) img->imageData, img->height, img->width, min, max, (unsigned char*) image_sse->imageData);
+    cvShowImage( "Affiche_sse", image_sse);
+
 
 
 
